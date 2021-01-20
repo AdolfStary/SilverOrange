5fc641d71c2816374b1b63c1711dcd893f23ce96
AdolfStary <adolf.stary@hotmail.com>
2021-01-20 08:59:56 -0700

    Applied prettier to repos.ts;

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -7,27 +7,23 @@ export const repos = Router();
 repos.get('/', async (_: Request, res: Response) => {
   res.header({
     'Cache-Control': 'no-store',
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
   });
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  try{
+  try {
     const githubRepos = await getGithubRepos();
     let mergedRepos = githubRepos.concat(jsonFileRepos);
-    
+
     // Filters Repos
-    const filteredRepos = mergedRepos.filter((repo:any) => {
+    const filteredRepos = mergedRepos.filter((repo: any) => {
       return repo.fork === false;
     });
-  
+
     res.status(200);
-    res.json(filteredRepos);  
-  }
-  catch (err){
+    res.json(filteredRepos);
+  } catch (err) {
     res.status(400);
     res.json(err);
   }
-
 });
-
-
