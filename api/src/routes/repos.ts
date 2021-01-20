ecf8504082242519cf7b9e6b23e181477ae324f8
AdolfStary <adolf.stary@hotmail.com>
2021-01-19 20:47:50 -0700

    UPDATED /repos path to return filtered JSON data from file and github;

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -5,16 +5,33 @@ import axios from 'axios';
 export const repos = Router();
 
 repos.get('/', async (_: Request, res: Response) => {
-  res.header('Cache-Control', 'no-store');
-
-  res.status(200);
+  res.header({
+    'Cache-Control': 'no-store',
+    'Content-Type': 'application/json'
+  });
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json(jsonFileRepos);
-  
+  //res.json({...githubRepos, jsonFileRepos});
+
+  const githubRepos = await getGithubRepos();
+  let mergedRepos = githubRepos.concat(jsonFileRepos);
+
+  const filteredRepos = mergedRepos.filter((repo:any) => {
+    return repo.fork === false;
+  });
+
+  res.status(200);
+  res.json(filteredRepos);  
 });
 
 const getGithubRepos = async () => {
-
-
+  const repos = await axios(
+    {
+      method: 'get',
+       url: 'https://api.github.com/users/silverorange/repos'
+    }
+  ).then((res) => {
+    return res.data;
+  });
+  return repos;
 }
